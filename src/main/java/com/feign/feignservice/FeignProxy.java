576bbedb29077c8d9758234347471597452df3f4
manvendrasingh <manvendra80386@gmail.com>
2020-06-21 22:11:48 +0530

    Added Feign Client and RibbonConfiguration

--- src/main/java/com/feign/feignservice/FeignProxy.java
+++ src/main/java/com/feign/feignservice/FeignProxy.java
@@ -1,8 +1,11 @@
 package com.feign.feignservice;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "exchange-service",url = "localhost:8081")
+//@FeignClient(name = "exchange-service",url = "localhost:8081")
+@FeignClient(name = "exchange-service")
+@RibbonClient(name = "exchange-service")
 public interface FeignProxy {
   @GetMapping("/exchange")
   public String portInfo();
